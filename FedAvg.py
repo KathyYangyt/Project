571c4c775541160f67832a7e2040f3354fda4bd0
‘KathyYangyt’ <2538069Y@student.gla.ac.uk>
2022-09-08 15:47:04 +0800

    modify FedAvg and options

--- FedAvg.py
+++ FedAvg.py
@@ -1,16 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Python version: 3.6
-
 import copy
 import torch
-from torch import nn
 
 
 def FedAvg(weight_list):
     weight_avg = copy.deepcopy(weight_list[0])
-    for w in weight_avg.keys():
+    for k in weight_avg.keys():
         for i in range(1,len(weight_list)):
-            weight_avg[w] += weight_list[i][w]
-        weight_avg[w] = torch.div(weight_avg[w], len(weight_list))
+            weight_avg[k] += weight_list[i][k]
+        weight_avg[k] = weight_avg[k]/len(weight_list)
     return weight_avg
